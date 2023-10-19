d11ca3c41ea63228b2483b48516b8da68fcd8a6e
Rishabh Gupta <rishabh9aug@gmail.com>
2023-10-19 20:23:32 +0530

    basic done

--- app/src/main/java/com/example/databindingrevision/MainActivity.kt
+++ app/src/main/java/com/example/databindingrevision/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.databindingrevision
 
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databindingrevision.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+       binding.textView1.text="People are like rivers"
+        binding.textView2.text="Top G"
+
+
+
+
+
     }
 }
